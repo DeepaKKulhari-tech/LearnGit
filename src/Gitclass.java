ecbf181723653ca6eb599eea8fa05b358a1f7718
DeepaKKulhari-tech <montuchandna@gmail.com>
2021-06-14 18:47:34 +0530

    second commit

--- src/Gitclass.java
+++ src/Gitclass.java
@@ -5,6 +5,8 @@ public class Gitclass {
 		// TODO Auto-generated method stub
 
 		System.out.println("Test Git class 1");
+		System.out.println("New chnages");
+		System.out.println("Chnages");
 	}
 
 }
